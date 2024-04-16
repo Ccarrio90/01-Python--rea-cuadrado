0c0c1ddacf4093715af9c4f5691608da22d0bf96
Ccarrio90 <166958189+Ccarrio90@users.noreply.github.com>
2024-04-16 01:16:12 -0300

    Create ÁreaCuadrado.py

---
+++ ÁreaCuadrado.py
@@ -0,0 +1,12 @@
+# 1 - Solicitar la longitud del lado
+def area_cuadrado(lado):
+    
+    #2 - Calcular el área
+    area = lado * lado
+
+    #3 - Mostrar el resultado
+    print(f"El área del cuadrado con lado {lado}, es de {area} cm.")
+
+area_cuadrado(23)
+area_cuadrado(12)
+area_cuadrado(23.76)
